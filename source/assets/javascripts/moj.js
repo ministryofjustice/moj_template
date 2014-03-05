7f84eb1e287ba69263130b05349f966f7fc9e273
Mat Harden <matharden@gmail.com>
2014-03-05 16:05:20 +0000

    Update blog URL in JavaScript

--- source/assets/javascripts/moj.js
+++ source/assets/javascripts/moj.js
@@ -45,7 +45,7 @@
       var m = '      ___          ___       ___\n     /__/\\        /  /\\     /  /\\\n    |  |::\\      /  /::\\   /  /:/\n    |  |:|:\\    /  /:/\\:\\ /__/::\\\n  __|__|:|\\:\\  /  /:/  \\:\\\\__\\/\\:\\\n /__/::::| \\:\\/__/:/ \\__\\:\\  \\  \\:\\\n \\  \\:\\~~\\__\\/\\  \\:\\ /  /:/   \\__\\:\\\n  \\  \\:\\       \\  \\:\\  /:/    /  /:/\n   \\  \\:\\       \\  \\:\\/:/    /__/:/\n    \\  \\:\\       \\  \\::/     \\__\\/\n     \\__\\/        \\__\\/',
       txt = '\n\nLike what you see? Want to make a difference?'+
             '\n\nFind out how we\'re making the Ministry Of Justice Digital by Default.'+
-            '\nhttp://blogs.justice.gov.uk/digital/.'+
+            '\nhttps://mojdigital.blog.gov.uk/.'+
             '\n\nGet in touch to see what positions are available and see what projects you could be working on.'+
             '\nhttps://twitter.com/MOJDigital/statuses/413340917509001216';
       moj.log(m+txt);
