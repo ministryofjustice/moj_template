ce10c441e8fc12dd766ba003f8447a12fd274a34
Dom Smith <dom@teneightfive.com>
2014-03-07 10:21:26 +0000

    Add github and pypi creds

--- travis.sh
+++ travis.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-echo "Setting API Key"
+echo "Setting API Keys"
+
+# Rubygems
 touch ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 echo "---" >> ~/.gem/credentials
-echo ":rubygems_api_key: $RUBYGEMS" >> ~/.gem/credentials
+echo ":rubygems_api_key: ${RUBYGEMS_API}" >> ~/.gem/credentials
+
+# PiPy
+touch ~/.pypirc
+chmod 0600 ~/.pypirc
+echo "[distutils]" >> ~/.pypirc
+echo "index-servers =" >> ~/.pypirc
+echo "    pypi" >> ~/.pypirc
+echo "" >> ~/.pypirc
+echo "[pypi]" >> ~/.pypirc
+echo "username:${PYPI_USER}" >> ~/.pypirc
+echo "password:${PIPY_PASS}" >> ~/.pypirc
+
+# Github
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
